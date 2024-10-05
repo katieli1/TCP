2851ecc49e208f8e61bb7b32ed535b0f44b589b9
kli4 <katie_li@outlook.com>
2024-10-05 16:33:41 -0400

    feat: sendIP

--- pkg/lookup/lookup.go
+++ pkg/lookup/lookup.go
@@ -6,19 +6,20 @@ import (
 	"github.com/google/netstack/tcpip/header"
 	"ip/pkg/header-parser"
 	"ip/pkg/lnxconfig"
+	"net"
 	"net/netip"
 	"os"
 )
 
-// type Interface struct {
-// 	IpPrefix netip.Prefix
-// 	IpAddr   netip.Addr
-// 	UdpConn  netip.AddrPort
-// 	Name     string
-// }
+type Interface struct {
+	IpPrefix    netip.Prefix
+	IpAddr      netip.Addr
+	UdpAddrPort netip.AddrPort
+	UdpConn     net.Conn
+	Name        string
+}
 
-var interfaces []lnxconfig.InterfaceConfig
-var lookupTable = make(map[netip.Addr]*lnxconfig.InterfaceConfig)
+var lookupTable = make(map[netip.Addr]*Interface)
 
 func main() {
 	if len(os.Args) != 2 {
@@ -40,14 +41,30 @@ func populateTable(fileName string) {
 	// Populate lookup table
 	for _, iface := range lnxConfig.Interfaces {
 		prefixForm := netip.PrefixFrom(iface.AssignedIP, iface.AssignedPrefix.Bits())
-		//i := &lnxconfig.InterfaceConfig{Name: iface.Name, IpPrefix: prefixForm, IpAddr: prefixForm.Addr(), UdpConn: iface.UDPAddr}
-		lookupTable[prefixForm.Addr()] = &iface
-		interfaces = lnxConfig.Interfaces
+		i := &Interface{Name: iface.Name, IpPrefix: prefixForm, IpAddr: prefixForm.Addr(), UdpAddrPort: iface.UDPAddr}
+		lookupTable[prefixForm.Addr()] = i
+		createUdpConn(i)
 	}
 
 }
 
-func sendIP(dest netip.Addr, protocolNum int, data []byte) error {
+func createUdpConn(iface *Interface) {
+	addrPort := iface.UdpAddrPort
+	udpAddr := &net.UDPAddr{
+		IP:   addrPort.Addr().AsSlice(), // Convert netip.Addr to net.IP
+		Port: int(addrPort.Port()),      // Get the port from AddrPort
+	}
+
+	// Create a UDP connection (for sending)
+	conn, err := net.DialUDP("udp", nil, udpAddr)
+	if err != nil {
+		fmt.Println("Error dialing UDP connection:", err)
+		return
+	}
+	iface.UdpConn = conn
+}
+
+func SendIP(dest netip.Addr, protocolNum int, data []byte) error {
 	//1. is the packet valid? is checksum and TTL 0? if no, drop
 	header, err := ipv4header.ParseHeader(data)
 	if err != nil {
@@ -67,21 +84,17 @@ func sendIP(dest netip.Addr, protocolNum int, data []byte) error {
 
 	// 2. is the packet for me (based on dest address)? If it’s one of “your” IPs, send up to OS
 	message := data[headerSize:]
-	for i := range len(interfaces) {
-		if dest == interfaces[i].AssignedIP {
-			// yay
-		}
+	iface, exists := lookupTable[dest]
+	if exists {
+		iface.UdpConn.Write(message)
+	} else { // 4. if no match anywhere, drop the packet and return an error
+		return errors.New("destination IP is not known")
 	}
-	fmt.Println(message) // so Go doesn't kill us
 
-	// 3. if match for local network in forwarding table send to that network. if match for next hop IP, consult table again, and see if you have local match for that IP
-
-	// 4. if no match anywhere, drop the packet and return an error
 	return nil
 }
 
 func ValidateChecksum(b []byte, fromHeader uint16) uint16 {
 	checksum := header.Checksum(b, fromHeader)
-
 	return checksum
 }

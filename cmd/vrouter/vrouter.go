734d2c529c589f65d9f0ad7a0e0a4bc7343c0e94
kli4 <katie_li@outlook.com>
2024-10-11 20:16:34 -0400

    router code

--- cmd/vrouter/vrouter.go
+++ cmd/vrouter/vrouter.go
@@ -1,7 +1,23 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"ip/pkg/lookup"
+	"os"
+)
 
+func callback(msg string) {
+	fmt.Println("Received test packet: " + msg)
+
+}
 func main() {
+	fmt.Println("In main!")
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage:  %s <configFile>\n", os.Args[0])
+		os.Exit(1)
+	}
+	fileName := os.Args[1]
+	lookup.RegisterRecvHandler(0, callback)
+	lookup.Initialize(fileName)
 
 }
